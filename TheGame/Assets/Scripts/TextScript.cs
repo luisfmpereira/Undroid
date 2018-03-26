a61bc9c2bcda315be4e64bf4fdcee86a71cbdb62
Gabrieltmi <gabrieltameomaia@gmail.com>
2018-03-26 00:42:13 -0300

    Animação + sprite
    Arrumei a animação coloquei uma transição direto do pular pro correr, e tentei colocar sprites para(Andar, pular e interagir) mas não está colidindo da uma olhada...

---
+++ TheGame/Assets/Scripts/TextScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TextScript : MonoBehaviour {
+	public GameObject textAD;
+	public GameObject textZ;
+	public GameObject textSPACE;
+	private Transform trans;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		trans = GetComponent<Transform> ();
+	}
+
+	public void OnCollisionStay2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Text1")
+			{
+			Debug.Log ("tocando");
+			GameObject z = GameObject.Instantiate (textAD, trans.position, Quaternion.identity);
+			}
+		if(col.gameObject.tag == "Text2")
+		{
+			GameObject z = GameObject.Instantiate (textZ, trans.position, Quaternion.identity);
+		}
+	}
+}
