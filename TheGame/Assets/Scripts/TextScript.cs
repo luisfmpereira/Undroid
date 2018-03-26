f1675237bc01e9c1870075b7017f8b8cc42b0ecc
Gabrieltmi <gabrieltameomaia@gmail.com>
2018-03-26 11:26:06 -0300

    Corrigido Trigger

--- TheGame/Assets/Scripts/TextScript.cs
+++ TheGame/Assets/Scripts/TextScript.cs
@@ -3,30 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class TextScript : MonoBehaviour {
+public class TextScript : MonoBehaviour
+{
+	public GameObject z;
 	public GameObject textAD;
 	public GameObject textZ;
 	public GameObject textSPACE;
 	private Transform trans;
+	public float y = 0.2f;
 
 
 
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		trans = GetComponent<Transform> ();
+
 	}
 
-	public void OnCollisionStay2D(Collision2D col)
+
+	public void OnTriggerEnter2D (Collider2D col)
 	{
-		if(col.gameObject.tag == "Text1")
-			{
-			Debug.Log ("tocando");
-			GameObject z = GameObject.Instantiate (textAD, trans.position, Quaternion.identity);
-			}
-		if(col.gameObject.tag == "Text2")
-		{
-			GameObject z = GameObject.Instantiate (textZ, trans.position, Quaternion.identity);
+		
+		if (col.gameObject.tag == "Text1") {
+		    z = GameObject.Instantiate (textAD, new Vector3(trans.transform.position.x, trans.transform.position.y + y, trans.transform.position.z), Quaternion.identity);
+			z.transform.parent = trans.transform;
+
+		}
+		if (col.gameObject.tag == "Text2") {
+			z = GameObject.Instantiate (textSPACE, new Vector3(trans.transform.position.x, trans.transform.position.y + y, trans.transform.position.z), Quaternion.identity);
+			z.transform.parent = trans.transform;
 		}
 	}
+
+	public void OnTriggerExit2D (Collider2D col)
+	{
+		z.transform.parent = null;
+		GameObject.Destroy (z, 0);
+
+	}
+
+
 }
