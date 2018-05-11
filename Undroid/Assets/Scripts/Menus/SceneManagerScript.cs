6634c0ffeb4f03b5ba158829ce2adb10fe241286
Luis Filipe Modesto Pereira <luisf.modestop@gmail.com>
2018-05-11 17:39:27 -0300

    menus

---
+++ Undroid/Assets/Scripts/Menus/SceneManagerScript.cs
@@ -0,0 +1,45 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManagerScript : MonoBehaviour {
+
+	public void LoadScene (string sceneName){
+
+		SceneManager.LoadScene (sceneName);
+
+	}
+
+	//quitting methods
+	public GameObject areYouSureQuit;
+
+
+	void Awake(){
+		areYouSureQuit.SetActive (false);
+
+	}
+
+	public void SureQuit(){
+		areYouSureQuit.SetActive (true);
+	}
+
+	public void SureQuitNo(){
+		areYouSureQuit.SetActive (false);
+	}
+
+	public void QuitGame(){
+		#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+}
