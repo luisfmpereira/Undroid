f9dd0384ecd78fa47aaa9fd7d9a4f8e09b06b975
Gabrieltmi <gabrieltameomaia@gmail.com>
2018-05-13 23:55:02 -0300

    level2

---
+++ Undroid/Assets/Scripts/Interactables/InteractRunningMachine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractRunningMachine : MonoBehaviour {
+	private bool interacted = false;
+
+	public GameObject runningmachine;
+
+
+	void Update(){
+			interacted = Input.GetButtonDown ("Fire1");
+
+	}
+
+	void OnCollisionStay2D (Collision2D hit){
+		if (hit.gameObject.CompareTag ("Player") && interacted) {
+			runningmachine.GetComponent<runningmachine> ().isrunning = true;
+		}
+	}
+}
