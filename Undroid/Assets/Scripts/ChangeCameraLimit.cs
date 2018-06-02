a6f2e827b6620b4da3fecf5ac616a582d5e5d108
Gabrieltmi <gabrieltameomaia@gmail.com>
2018-06-02 02:57:37 -0300

    ScreenAnimations
    NewScriptAdjustmentCamera

---
+++ Undroid/Assets/Scripts/ChangeCameraLimit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCameraLimit : MonoBehaviour {
+	public GameObject cam;
+	public new Vector2 maxXY;
+	public new Vector2 minXY;
+	public bool changeMax;
+	public bool changeMin;
+
+	void Start () {
+		cam = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.CompareTag("Player")){
+			if(changeMax)
+			cam.GetComponent<CameraFollow> ().maxXAndY = maxXY;
+			if(changeMin)
+			cam.GetComponent<CameraFollow> ().minXAndY = minXY;
+		}
+	}
+
+}
