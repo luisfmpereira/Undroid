61390d61c66ab04e84da45deda3e73310c6cc82b
Luis Filipe <luisf.modestop@gmail.com>
2018-06-02 00:24:13 -0300

    finished level5

---
+++ Undroid/Assets/Scripts/Enemies Scripts/BossLevel5.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLevel5 : Boss {
+
+	public Transform[] positions;
+	public float moveSpeed = 2f;
+	private int selectedPosition;
+	private float angleToMultiply = 120 * Mathf.Deg2Rad;
+	public GameObject laser;
+
+	void Start () {
+		selectedPosition = 0;
+	}
+
+	void Update () {
+		ShowLife ();
+		KillBoss ();
+		if (bossHealth <= 0)
+			laser.SetActive (false);
+
+		if (turnBossOn) {
+			
+			MoveEnemy ();
+
+			Shooting ();
+		}
+		
+	}
+
+	void MoveEnemy(){
+		this.transform.position = Vector2.MoveTowards(this.transform.position,positions[selectedPosition].position,moveSpeed*Time.deltaTime);
+
+
+		if (this.transform.position == positions [selectedPosition].position)
+			selectedPosition++;
+
+		if (selectedPosition == positions.Length)
+			selectedPosition = 0;
+	}
+
+
+	void Shooting(){
+
+		shootTimer -= Time.deltaTime;
+
+		if (shootTimer <= 0) {
+			Instantiate (bulletPrefab, this.transform.position, Quaternion.Euler (0, 0, -90)).AddForce (new Vector2 (0, -bulletForce));
+			Instantiate (bulletPrefab, this.transform.position, Quaternion.Euler(0,0,-75)).AddForce(new Vector2(bulletForce*Mathf.Cos(angleToMultiply/2),-bulletForce*Mathf.Sin(angleToMultiply/2)));
+			Instantiate (bulletPrefab, this.transform.position, Quaternion.Euler(0,0,-105)).AddForce(new Vector2(-bulletForce*Mathf.Cos(angleToMultiply/2),-bulletForce*Mathf.Sin(angleToMultiply/2)));
+			shootTimer = Random.Range (1f, 4f);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D hit){
+		DamageByPlayer (hit);
+	}
+}
---
+++ Undroid/Assets/Scripts/Interactables/CanonLevel5.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonLevel5 : MonoBehaviour {
+
+	public Rigidbody2D playerBullet;
+	public GameObject muzzle;
+	public float bulletSpeed;
+	public bool interacted;
+	private float timer;
+	public float cooldown = 4f;
+
+	public GameObject button;
+	public Sprite greenButton;
+	public Sprite redButton;
+
+	void Update () {
+		timer -= Time.deltaTime;
+		interacted = Input.GetButtonDown ("Fire1");
+		if (timer<=0)
+			button.GetComponent<SpriteRenderer> ().sprite = greenButton;
+	}
+
+
+	void OnTriggerStay2D(Collider2D hit){
+		if (hit.gameObject.CompareTag ("Player") && interacted && timer <= 0) {
+			button.GetComponent<SpriteRenderer> ().sprite = redButton;
+			Instantiate (playerBullet, muzzle.transform.position, Quaternion.Euler (0, 0, 90)).AddForce(new Vector2(0,bulletSpeed));
+			timer = cooldown;
+		}
+
+	}
+}
